846c8456588a8d45f3bb04b6707f7877bbe4c25c
John Lemuel G. Hapa <jl.g.hapa@gmail.com>
2023-02-22 09:00:23 +0800

    continue v3

--- lib/helpers/variables.dart
+++ lib/helpers/variables.dart
@@ -16,8 +16,7 @@ class AppColor {
 }
 
 class ApiConfig {
-  // static String baseUrl = 'http://203.177.49.45:8001';
-  static String baseUrl = 'http://10.11.79.19:3001';
+  static String baseUrl = 'https://pcari.mab.com.ph:8001';
 }
 
 class AppConfig {
